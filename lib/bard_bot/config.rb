7d7e1b15b3849d28a2f2e98d0d13ee6184d7091b
EvilScott <reis.robert.s@gmail.com>
2015-01-11 16:42:48 -0800

    add more config options

--- lib/bard_bot/config.rb
+++ lib/bard_bot/config.rb
@@ -1,9 +1,12 @@
 module BardBot
   class Config
-    attr_accessor :character
+    attr_accessor :character, :prefix, :max_length, :character_directory
 
     def initialize
       @character = :hamlet
+      @prefix = 2
+      @max_length = 100
+      @character_directory = :default
     end
 
   end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 module BardBot
   describe Config, '#initialize' do
     it 'creates a new config with defaults' do
-      config = Config.new.instance_variable_get(:@character)
-      expect(config).to eq :hamlet
+      config = Config.new
+      expect(config.instance_variable_get(:@character)).to eq :hamlet
+      expect(config.instance_variable_get(:@prefix)).to eq 2
+      expect(config.instance_variable_get(:@max_length)).to eq 100
+      expect(config.instance_variable_get(:@character_directory)).to eq :default
     end
   end
 end
